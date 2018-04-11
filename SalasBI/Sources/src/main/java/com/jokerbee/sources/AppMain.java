dee0eaba7d5d1280bca0a73591dda7179e4f6fa5
lostFatRivers <495669631@qq.com>
2018-04-11 22:19:01 +0800

    modify main

--- SalasBI/Sources/src/main/java/com/jokerbee/sources/AppMain.java
+++ SalasBI/Sources/src/main/java/com/jokerbee/sources/AppMain.java
@@ -2,8 +2,6 @@ package com.jokerbee.sources;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.shareddata.AsyncMap;
-import io.vertx.core.shareddata.SharedData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,19 +16,8 @@ public class AppMain {
         VertxOptions options = new VertxOptions();
         options.setWorkerPoolSize(10);
         Vertx vertx = Vertx.vertx(options);
-//        vertx.deployVerticle("com.jokerbee.sources.BootVerticle", res -> {
-//            if (res.succeeded()) {
-//                logger.info("BI sources collector start OK.");
-//            } else {
-//                logger.error("BI sources collector start error.", res.cause());
-//                //vertx.close();
-//            }
-//        });
-        SharedData data = vertx.sharedData();
-        data.getAsyncMap("Connectors", res -> {
+        vertx.deployVerticle("com.jokerbee.sources.BootVerticle", res -> {
             if (res.succeeded()) {
-                AsyncMap<Object, Object> result = res.result();
-                logger.info("shared map:{}", result);
                 logger.info("BI sources collector start OK.");
             } else {
                 logger.error("BI sources collector start error.", res.cause());
