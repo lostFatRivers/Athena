aad9dda2b2a381dbf478a71299e4a8ea039f3f63
Joker <495669631@qq.com>
2020-01-21 12:49:33 +0800

    delete no use pro

--- SalasBI/Sources/src/main/java/com/jokerbee/sources/common/CommonFieldName.java
+++
@@ -1,8 +0,0 @@
-package com.jokerbee.sources.common;
-
-/**
- * 通用字段名称字典;
- */
-public class CommonFieldName {
-    public static final String SERVER_ID = "serverId";
-}
--- SalasBI/Sources/src/main/java/com/jokerbee/sources/protocol/InnerMessage.java
+++
@@ -1,5 +0,0 @@
-package com.jokerbee.sources.protocol;
-
-public class InnerMessage {
-
-}
